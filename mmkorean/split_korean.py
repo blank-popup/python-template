46a6e4bb43c7f4baff5f1e687674fd89a1815154
blank-popup <ha6446@gmail.com>
2022-09-21 22:58:27 +0900

    한글 분리 예제 추가

---
+++ mmkorean/split_korean.py
@@ -0,0 +1,6 @@
+import mmkorean.korean
+
+if __name__ == '__main__':
+    value = mmkorean.korean.split_korean_consonant_vowel('아름다운')
+    print(value)
+    mmkorean.korean.print_korean_unicode_value()
